c66a349a557a8bed94f0d2946a6354b709cdf7b7
T-dithyrambuds <1952525717@qq.com>
2020-03-01 23:20:46 +0800

    修复一个执行时若遇到重复数据，计数失效的bug

--- homework.py
+++ homework.py
@@ -11,30 +11,37 @@ except Exception as e:
     pass
 
 
+
 def print_text(url, typ, num):
     l = []
-    j = 1
+
     while True:
         r = requests.get(url, params={'from': 'liuting'})
-
-        if r.text not in l:
-            print(j, r.text)
+        result_data = r.text
 
         sql = 'SELECT COUNT(id) FROM mywork WHERE item = ?'
-        value = conn.execute(sql, (r.text,)).fetchall()
-        value = value[0][0]
+        result = conn.execute(sql, (result_data,)).fetchall()
+        result = result[0][0]
 
-        if value != 0:
-            print('    发现重复的啦~')
+        if result > 0:  # 如果result大于0表示发现重复 跳过执行
             continue
 
+        # 如果没有重复内容 则继续执行下面
+        # 如果上面有重复内容，本身是没有做任何操作的，只是跳过，没有插入数据库，所以不用做计数以及调试查看
+
         sql = 'INSERT INTO mywork (type, item) VALUES (?, ?)'
-        cursor.execute(sql, (typ, r.text))
-        conn.commit()
-        j = j + 1
-        l.append(r.text)
-        if j >= num + 1:
+        cursor.execute(sql, (typ, result_data))
+        conn.commit()  # 提交到数据库
+
+        # 插入了数据库 num 条内容后，跳出运行
+        l.append(result_data)
+
+        # 输出作为调试用
+        print(len(l), result_data)
+        if len(l) >= num:
             break
+
+
 
 
 print_text('https://chp.shadiao.app/api.php', '彩虹屁', 20)
